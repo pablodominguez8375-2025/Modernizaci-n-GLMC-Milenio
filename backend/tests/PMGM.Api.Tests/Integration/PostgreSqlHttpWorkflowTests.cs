5eee597290bf594f3ba201d3f4a36504ab18307e
pablodominguez8375-2025 <pablo.dominguez8375@gmail.com>
2026-09-07 13:30:01 -0300

    fix(http-tests): importar extensiones de DI

--- backend/tests/PMGM.Api.Tests/Integration/PostgreSqlHttpWorkflowTests.cs
+++ backend/tests/PMGM.Api.Tests/Integration/PostgreSqlHttpWorkflowTests.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
